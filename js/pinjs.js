0e06b552caef92f67318af3d1cc4083de880c90a
Jack Underwood <33754428+14underwoodj@users.noreply.github.com>
2018-06-08 09:30:20 +0100

    Advanced PIN

---
+++ js/pinjs.js
@@ -0,0 +1,49 @@
+jQuery(document).ready(function($) {
+  $(document).ready(function() {
+
+    var pin = (+!![] + []) + (!+[] + !![] + []) + (!+[] + !![] + !![] + []) + (!+[] + !![] + !![] + !![] + []);
+    var enterCode = "";
+    enterCode.toString();
+
+    $("#numbers button").click(function() {
+
+      var clickedNumber = $(this).text().toString();
+      enterCode = enterCode + clickedNumber;
+      var lengthCode = parseInt(enterCode.length);
+      lengthCode--;
+      $("#fields .numberfield:eq(" + lengthCode + ")").addClass("active");
+
+      if (lengthCode == 3) {
+
+        if (enterCode == pin) {
+          /*$("#fields .numberfield").addClass("right");
+          $("#numbers").addClass("hide");
+          $("#anleitung p").html("Correct. Entering...");*/
+            changeSection("0", "3");
+
+        } else {
+          $("#fields").addClass("miss");
+          enterCode = "";
+          setTimeout(function() {
+            $("#fields .numberfield").removeClass("active");
+          }, 200);
+          setTimeout(function() {
+            $("#fields").removeClass("miss");
+          }, 500);
+
+        }
+
+      } else {}
+
+    });
+    
+    $("#restartbtn").click(function(){
+      enterCode = "";
+      $("#fields .numberfield").removeClass("active");
+      $("#fields .numberfield").removeClass("right");
+      $("#numbers").removeClass("hide");
+      $("#anleitung p").html("<strong>Please enter the correct PIN-Code.</strong><br> It is: 1234 / Also try a wrong code");
+    });
+
+  });
+});
