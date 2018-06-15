05d839fa1e9c3d160245d1df86e2f054ba49398b
14zrobczynskij <39554504+14zrobczynskij@users.noreply.github.com>
2018-06-15 14:22:42 +0100

    pauses after 2 seconds

--- js/main.js
+++ js/main.js
@@ -95,7 +95,11 @@ function PlaySound() {
     var x = Math.floor(Math.random() * Sounds.length);
     audio.src = Sounds[x];
     audio.play();
-    setTimeout(audio.stop, 2000);
+    setTimeout(function(){
+        console.log("Stopping");
+        var audio = document.getElementById("audio");
+        audio.pause();
+    }, 2000);
 }
 
 $(".choosable").on("click",PlaySound);
